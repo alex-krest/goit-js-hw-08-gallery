3c96d7c65fb4b941f0636eb95ca8d0095369c8b3
alex-krest <73220512+alex-krest@users.noreply.github.com>
2021-05-07 10:34:13 +0300

    Исправлено

--- work-shop.js
+++ work-shop.js
@@ -45,32 +45,18 @@ function onClickOpenLightbox(evt) {
   //Подмена значения атрибута `src` элемента `img.lightbox__image`.
   lightboxImageEl.src = evt.target.dataset.source;
   lightboxImageEl.alt = evt.target.alt;
-  getImageUrlAndAlt(evt);
 }
 
 // ФУНКЦИИ ЗАКРЫТИЯ МОДАЛКИ
 function closeModal() {
-  window.removeEventListener("keydown", onEscKeyPress);
   //Очистка значения атрибута `src` элемента `img.lightbox__image`
   lightboxImageEl.src = "";
   lightboxEl.classList.remove("is-open");
 }
-function onEscKeyPress(evt) {
-  if (evt.code !== "Escape") {
-    return;
-  }
-  closeModal();
-}
-// ФУНКЦИЯ ЗАКРЫТИЯ МОДАЛКИ ПРИ КЛИКЕ НЕ НА КАРТИНКУ
-function onClickCloseModal(evt) {
-  if (evt.target !== evt.currentTarget) {
-    return;
-  }
-  closeModal();
-}
+
 //Закрытие модального окна по клику на кнопку
 // СЛУШАТЕЛЬ НА КНОПКУ ЗАКРЫТИЯ
-closeModalBtnEl.addEventListener("click", onClickCloseModal);
+closeModalBtnEl.addEventListener("click", closeModal);
 
 // СЛУШАТЕЛЬ НА ЗАКРЫТИЕ ПРИ КЛИКЕ НА ОВЕРЛЕЙ
-lightboxOverlayEl.addEventListener("click", onClickCloseModal);
+lightboxOverlayEl.addEventListener("click", closeModal);
